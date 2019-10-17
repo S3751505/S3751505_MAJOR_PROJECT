98480b883de92aee646e87a4c78ac38f21be62b7
S3751505 <56577279+S3751505@users.noreply.github.com>
2019-10-17 16:45:42 +1100

    Update QGIS_BIVARIATE_RENDERER.py

--- QGIS_BIVARIATE_RENDERER.py
+++ QGIS_BIVARIATE_RENDERER.py
@@ -1,7 +1,3 @@
-Python 3.6.8 (v3.6.8:3c6b436a57, Dec 24 2018, 02:04:31) 
-[GCC 4.2.1 Compatible Apple LLVM 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
 
 #//QGIS BIVARIATE RENDERER OCTOBER 2019 Mitchell Younes RMIT//
 
